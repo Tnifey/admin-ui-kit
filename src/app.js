10753dfb2fcd6c2d1dab37b2deadc1ed8ad82740
Tnifey <tnifey@gmail.com>
2017-09-27 15:23:55 +0200

    Some sort of components

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-// smth
+import './development-styles.sass';
+import './components/components.sass';
+import test from './test.html';
+document.body.innerHTML = '<div class="playground">'+ test +'</div>' + document.body.innerHTML;
